dd6e5bcba72017a2073329cd0c82b1d5106d7b89
@jiqiaochun <13011251196@163.com>
2016-04-19 09:25:26 +0800

    Initial Commit

---
+++ Emation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Emation
+//
+//  Created by 姬巧春 on 16/4/19.
+//  Copyright © 2016年 姬巧春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Emation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Emation
+//
+//  Created by 姬巧春 on 16/4/19.
+//  Copyright © 2016年 姬巧春. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
